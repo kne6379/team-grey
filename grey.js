bd1305b76719e30f18c2bcaa5c59575fa00edbc1
yun <dldbstjd3210@naver.com>
2024-04-16 10:57:47 +0900

    팀원명 수정

--- grey.js
+++ grey.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function () {
-  $("#savebtn").click(function () {
+  // $("#savebtn").click(function () {
+    $(".btn").click(function (e) {
+      console.log(e.target)
     $('#팀원상세정보').toggle();
+    
   });
 });
+
