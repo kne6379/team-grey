1f89aa5f1f2431a8b1b03b68a62405017f087731
jele <ju83144@gmail.com>
2024-04-16 17:43:18 +0900

    삭제 기능 추가

--- grey.js
+++ grey.js
@@ -1,8 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { collection, addDoc, } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -28,6 +26,7 @@ $("#maketextcard").click(async function () {
     "text": text
   };
   await addDoc(collection(db, "TeamProject"), doc);
+
 });
 
 let docs = await getDocs(collection(db, "TeamProject"));
@@ -37,17 +36,35 @@ docs.forEach((doc) => {
 
   let name_row = row["name"];
   let text_row = row["text"];
+  let postId = doc.id;
 
   let temp_html = `
     <div class="card border-dark mb-3" style="max-width: 1000rem;">
       <div class="card-body">
         <h5 class="card-title">${name_row}</h5>
         <p class="card-text">${text_row}</p>
-        <button type="button" class="btn btn-outline-dark">삭제</button><button type="button"
-          class="btn btn-outline-dark">수정</button>
+        <button type="button" class="btn btn-outline-dark delposting" data-id="${postId}">삭제</button>
+        <button type="button" class="btn btn-outline-dark">수정</button>
       </div>
     </div>`
     $("#cardbox").append(temp_html);
+});
+
+
+$(document).on("click", ".delposting", async function () {
+  const postId = $(this).data("id"); 
+
+  const ok = window.confirm("삭제하시겠습니까?");
+  if (ok) {
+    try {
+      await deleteDoc(doc(db, "TeamProject", postId)); 
+      $(this).closest(".card").remove(); 
+      alert("게시물이 성공적으로 삭제되었습니다.");
+    } catch (error) {
+      console.error("게시물 삭제 중 오류 발생:", error);
+      alert("게시물 삭제 중 오류가 발생했습니다. 다시 시도해주세요.");
+    }
+  }
 });
 
 
