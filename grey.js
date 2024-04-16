8615b0e3102eeb8e40eb8e91e27d5d3765c42c93
jeonsuwon <swjeon0330@gmail.com>
2024-04-16 11:08:36 +0900

    게시물 통합본

--- grey.js
+++ grey.js
@@ -4,3 +4,30 @@ $(document).ready(function () {
     $('#팀원상세정보').toggle();
   });
 });
+
+function maketextcard() {
+  let name = $('#name').val();
+  let text = $('#text').val();
+
+  if (($('#name').val().length === 0) || ($('#text').val().length === 0)) {
+    alert('닉네임 또는 방명록을 작성해주세요.');
+  } else {
+    let temp_html = `      
+    <div class="card border-dark mb-3" style="max-width: 1000rem;">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${text}</p>
+        <button type="button" class="btn btn-outline-dark">삭제</button><button type="button"
+        class="btn btn-outline-dark">수정</button>
+      </div>
+    </div>`
+    $("#cardbox").append(temp_html);
+  }
+}
+
+function deletebtn() {
+  $("#cardbox").empty();
+}
+
+
+
