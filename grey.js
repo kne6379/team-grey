126be0e6c11eb58f3ea6c6e423519dc8ffc646bc
jeonsuwon <swjeon0330@gmail.com>
2024-04-16 16:16:00 +0900

    데이터베이스 추가

--- grey.js
+++ grey.js
@@ -19,44 +19,50 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-$("#postingbtn").click(async function () {
+$("#maketextcard").click(async function () {
   let name = $("#name").val();
   let text = $("#text").val();
 
-  let doc = { name: name, text: text };
+  let doc = {
+    "name": name,
+    "text": text
+  };
   await addDoc(collection(db, "TeamProject"), doc);
 });
 
-$(document).ready(function () {
-  // $("#savebtn").click(function () {
-    $(".btn").click(function (e) {
-      console.log(e.target)
-    $('#팀원상세정보').toggle();
-    
-  });
-});
+let docs = await getDocs(collection(db, "TeamProject"));
+docs.forEach((doc) => {
+  let row = doc.data();
+  console.log(row);
 
-function maketextcard() {
-  let name = $('#name').val();
-  let text = $('#text').val();
+  let name_row = row["name"];
+  let text_row = row["text"];
 
-  if (($('#name').val().length === 0) || ($('#text').val().length === 0)) {
-    alert('닉네임 또는 방명록을 작성해주세요.');
-  } else {
-    let temp_html = `      
+  let temp_html = `
     <div class="card border-dark mb-3" style="max-width: 1000rem;">
       <div class="card-body">
-        <h5 class="card-title">${name}</h5>
-        <p class="card-text">${text}</p>
+        <h5 class="card-title">${name_row}</h5>
+        <p class="card-text">${text_row}</p>
         <button type="button" class="btn btn-outline-dark">삭제</button><button type="button"
-        class="btn btn-outline-dark">수정</button>
+          class="btn btn-outline-dark">수정</button>
       </div>
     </div>`
     $("#cardbox").append(temp_html);
-  }
-}
+});
 
-$("#cardbox").empty();
-function deletebtn() {
-}
+
+// $("#savebtn").click(function () {
+$(".btn").click(function (e) {
+  console.log(e.target)
+  $('#팀원상세정보').toggle();
+
+});
+
+
+  //if (($('#name').val().length === 0) || ($('#text').val().length === 0)) {
+  //  alert('닉네임 또는 방명록을 작성해주세요.');
+  //} else {
+  //}
+
+
 
